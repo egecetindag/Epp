c9255e5b65171d1f352e0b831e68e2bde384b59c
egece <egecetindag@hotmail.com>
2017-04-28 17:15:30 +0300

    updated

--- app/src/main/java/com/example/ege/epp/MainActivity.java
+++ app/src/main/java/com/example/ege/epp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.ege.epp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,21 +18,43 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends ActionBarActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.squareup.picasso.Picasso;
+
+import org.opencv.core.Mat;
+import java.util.Vector;
+
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+
+public class MainActivity extends AppCompatActivity {
 
     Spinner spinner1;
     Button button;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButton();
-        spinner1 = (Spinner)findViewById(R.id.spinner1);
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.select_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter);
         spinner1.setPrompt("Select");
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
     public void addListenerOnButton() {
 
         final Context context = this;
@@ -39,14 +67,13 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View arg0) {
 
                 Intent intent = new Intent(context, activity2.class);
-                startActivity(intent);
+               startActivity(intent);
 
             }
 
         });
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,5 +95,41 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
--- app/src/main/java/com/example/ege/epp/activity2.java
+++ app/src/main/java/com/example/ege/epp/activity2.java
@@ -5,12 +5,23 @@ package com.example.ege.epp;
  */
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.Context;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -18,129 +29,253 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt4;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import android.view.View.OnClickListener;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import static android.R.attr.data;
+import static android.R.attr.lines;
+import static java.lang.Math.cos;
+import static java.lang.Math.round;
+import static java.lang.Math.sin;
 import static org.opencv.core.Core.flip;
-public class activity2 extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+import static org.opencv.imgproc.Imgproc.COLOR_RGB2GRAY;
+import static org.opencv.imgproc.Imgproc.Canny;
+import static org.opencv.imgproc.Imgproc.GaussianBlur;
+import static org.opencv.imgproc.Imgproc.HoughLines;
+import static org.opencv.imgproc.Imgproc.HoughLinesP;
+import static org.opencv.imgproc.Imgproc.cvtColor;
+import static org.opencv.imgproc.Imgproc.line;
+import static org.opencv.imgproc.Imgproc.medianBlur;
+
+public class activity2 extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
 
-        private static String TAG = "MainActivity";
-        JavaCameraView javaCameraView;
-        Mat mRgba;
-        Mat mRgbaT;
-        Mat mRgbaF;
+    private static String TAG = "MainActivity";
+    JavaCameraView javaCameraView;
+    Mat mRgba;
+    Mat mRgbaT;
+    Mat mRgbaS;
+    Mat mRgbaF;
+    Mat edge;
+    Mat tmp;
+    TextView txt;
+    int screenOrientation = 0;
+    Mat line;
+    Point pt1, pt2,pt3,pt4;
+    ArrayList<String> f = new ArrayList<String>();// list of file paths
+    File[] listFile;
+    Vector<Mat> data;
 
-        static {
-            if(OpenCVLoader.initDebug()){
-                Log.i(TAG,"Opencv loaded successfully");
-            }
-            else{
-                Log.i(TAG,"Opencv not loaded");
-            }
-        }
 
-        BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
-            @Override
-            public void onManagerConnected(int status) {
-                switch (status) {
-                    case BaseLoaderCallback.SUCCESS:{
-                        javaCameraView.enableView();
-                        javaCameraView.setMaxFrameSize(200,200);
-                        break;
-                    }
-                    default:{
-                        super.onManagerConnected(status);
-                        break;
-                    }
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:{
+
+                    Display display = ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+                    screenOrientation = display.getRotation();
+                    if(screenOrientation == 0)
+                        javaCameraView.setCameraAngle(90);
+                    else if(screenOrientation == 1)
+                        javaCameraView.setCameraAngle(0);
+                    else if(screenOrientation == 3)
+                        javaCameraView.setCameraAngle(180);
+                    javaCameraView.enableView();
+                    break;
                 }
-                super.onManagerConnected(status);
+                default: {
+                    super.onManagerConnected(status);
+                    break;
+                }
             }
-        };
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main2);
-
-
-            javaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
-            javaCameraView.setVisibility(SurfaceView.VISIBLE);
-            javaCameraView.setCvCameraViewListener(this);
-
-
+            super.onManagerConnected(status);
         }
+    };
 
-        @Override
-        protected void onPause(){
-            super.onPause();
-            if(javaCameraView!=null)
-                javaCameraView.disableView();
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        @Override
-        protected void onDestroy(){
-            super.onDestroy();
-            if(javaCameraView!=null)
-                javaCameraView.disableView();
-        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main2);
 
-        @Override
-        protected void onResume(){
-            super.onResume();
-            if(OpenCVLoader.initDebug()) {
-                Log.i(TAG, "Opencv loaded successfully");
-                mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-            }
-            else {
-                Log.i(TAG, "Opencv not loaded");
-                OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0,this,mLoaderCallBack);
-            }
-        }
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
 
-        @Override
-        public void onCameraViewStarted(int width, int height) {
-            mRgba = new Mat(height, width, CvType.CV_8UC4);
-            mRgbaT = new Mat(height, width, CvType.CV_8UC4);
-            mRgbaF = new Mat(height, width, CvType.CV_8UC4);
-        }
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
 
-        @Override
-        public void onCameraViewStopped() {
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setCvCameraViewListener(this);
+javaCameraView.setBackgroundColor(5);
 
-        }
 
-        @Override
-        public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-            mRgba = inputFrame.rgba();
-            Core.transpose(mRgba,mRgbaT);
-            Imgproc.resize(mRgbaT,mRgbaF,mRgbaF.size(),0,0,0);
-            Display display = ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-            int screenOrientation = display.getRotation();
 
-            switch (screenOrientation%360) {
-                case 0:
-                    Core.flip(mRgbaF, mRgba, 1);
-                    //textView.setText("0degree");
-                    break;
-                case 90:
-                    Core.flip(mRgbaF, mRgba, 1);
-                    // textView.setText("90degree");
-                    break;
-                case 180:
-                    Core.flip(mRgbaF, mRgba, -1);
-                    break;
-                case 270:
-                    Core.flip(mRgbaF, mRgba, 0);
-                    //textView.setText("270degree");
-                    break;
-                default:
-                    break;
-            }
-            return mRgba;
 
-        }
 
     }
+
+    public void getFromSdcard()
+    {
+
+        InputStream stream = null;
+        Uri uri = Uri.parse("android.resource://com.example.ege.epp/drawable/kimbyeong");
+        try {
+            stream = getContentResolver().openInputStream(uri);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+        bmpFactoryOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+        Bitmap bmp = BitmapFactory.decodeStream(stream, null, bmpFactoryOptions);
+        tmp= new Mat();
+        Utils.bitmapToMat(bmp,tmp);
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (javaCameraView != null)
+            javaCameraView.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (javaCameraView != null)
+            javaCameraView.disableView();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (OpenCVLoader.initDebug()) {
+            Log.i(TAG, "Opencv loaded successfully");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            Log.i(TAG, "Opencv not loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallBack);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        getFromSdcard();
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaT = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaS= new Mat(height, width, CvType.CV_8UC4);
+        line= new Mat(height, width, CvType.CV_8UC4);
+         edge=new Mat();
+        javaCameraView.setBackgroundColor(2);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+
+        cvtColor(mRgba, mRgbaS,COLOR_RGB2GRAY);
+
+        //Applying Canny
+     Canny(mRgbaS,mRgbaS,80,90);
+        HoughLines(mRgbaS, line, 1, Math.PI / 180, 150);
+
+
+        int min1 = 80000, min2 = 80000, max1 = 0, max2 = 0;
+        if(line.cols()>=2) {
+            double[] data;
+            double rho, theta;
+            pt1 = new Point();
+            pt2 = new Point();
+            pt3 = new Point();
+            pt4 = new Point();
+            double a, b;
+            double x0, y0;
+
+                data = line.get(0, 0);
+                rho = data[0];
+                theta = data[1];
+                a = Math.cos(theta);
+                b = Math.sin(theta);
+                x0 = a * rho;
+                y0 = b * rho;
+                pt1.x = Math.round(x0 + 1000 * (-b));
+                pt1.y = Math.round(y0 + 1000 * a);
+                pt2.x = Math.round(x0 - 1000 * (-b));
+                pt2.y = Math.round(y0 - 1000 * a);
+            line(mRgba,pt1,pt2,new Scalar(0,255,0));
+                data = line.get(0, 1);
+                rho = data[0];
+            }
+
+
+         ///////////////////////////////////////
+
+
+
+       mRgba=mRgba.adjustROI(min2,max2,min1,max1);
+
+
+
+
+        return mRgba;
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
 
